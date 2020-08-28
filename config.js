c3cbc52a0c68b1afb17a7552d66c1e1e7047551f
Syarif <syarifuddin.studio@gmail.com>
2020-08-28 10:41:33 +0800

    update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const config={
-  baseUrl: 'http://localhost/wordpress/',
-  ck: 'ck_2651b8329cc3260960f6c3ca8d9aed8e174bb261',
-  cs: 'cs_aaa4f97dabc4a341025ef0e81529ddc447fa3d69'
+export const config = {
+  baseUrl: "http://104.196.3.96/",
+  ck: "ck_2651b8329cc3260960f6c3ca8d9aed8e174bb261",
+  cs: "cs_aaa4f97dabc4a341025ef0e81529ddc447fa3d69"
 }
