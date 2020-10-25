11a68f232217e4b3f78fac40ddef2fb4d3251557
Syarif <syarifuddin.studio@gmail.com>
2020-10-25 09:33:56 +0800

    Support web view on text component

--- src/Components/Text.js
+++ src/Components/Text.js
@@ -1,7 +1,28 @@
 import React from "react"
-import { Text as TextUi } from "react-native-elements"
+import { Text as TextUi } from "react-native"
 import HTML from "react-native-render-html"
 import { usePostContent } from "../Hook"
+import AutoHeightWebView from "react-native-autoheight-webview"
+
+const generateHtml = (content) => `
+    <style type="text/css">
+      table {
+        background-color: transparent;
+        width: 100%;
+        margin-bottom: 15px;
+        font-size: .9em;
+        border-spacing: 0;
+        border-collapse: collapse;
+      }
+      table td, table th {
+        padding: 15px;
+        line-height: 1.5;
+        vertical-align: top;
+        border: 1px solid #ccc;
+      }
+    </style>
+    ${content}
+  `
 
 export const Text = ({ title, postContent, ...props }) => {
   const content = usePostContent(postContent)
@@ -15,14 +36,29 @@ export const Text = ({ title, postContent, ...props }) => {
   const { style, ...restProps } = props
 
   return props.isHtml ? (
-    <HTML
-      html={title}
-      tagsStyles={{
-        span: style,
-        p: style,
-        del: { textDecorationLine: "line-through" }
-      }}
-    />
+    props.isWebView ? (
+      <AutoHeightWebView
+        style={{
+          ...style,
+          width: "100%"
+        }}
+        scalesPageToFit={false}
+        originWhitelist={["*"]}
+        source={{
+          html: generateHtml(title)
+        }}
+        viewportContent={"width=device-width, user-scalable=no"}
+      />
+    ) : (
+      <HTML
+        html={title}
+        tagsStyles={{
+          span: style,
+          p: style,
+          del: { textDecorationLine: "line-through" }
+        }}
+      />
+    )
   ) : (
     <TextUi style={style} {...restProps}>
       {title}
