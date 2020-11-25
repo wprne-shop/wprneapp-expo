fd8983859215b0ce131d7f2b947a6ba29cc6fd40
wprne-shop <wprne-shop@gmail.com>
2020-11-25 11:48:45 +0800

    update config

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 export const config = {
-  // baseUrl: window.wprneLocalize.baseUrl
-  baseUrl: "https://wprne.xyz/"
-  // baseUrl: "http://localhost/wordpress/",
-  // ck: "ck_2651b8329cc3260960f6c3ca8d9aed8e174bb261",
-  // cs: "cs_aaa4f97dabc4a341025ef0e81529ddc447fa3d69"
+	"baseUrl": "https://shop.wprne.xyz/",
+	"ck": "ck_495ca8413a9125db2887deff0ce88e5a4e22c624",
+	"cs": "cs_3c74350e0692ff09046e77371e1ab6aefed2fb99"
 }
